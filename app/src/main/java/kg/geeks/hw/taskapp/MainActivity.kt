d78b7dddcc80c7c444e408eceb2feb81d67743b4
Something <somethingforthismail@gmail.com>
2023-03-19 13:06:02 +0600

    Initial commit Code from homework 1

---
+++ app/src/main/java/kg/geeks/hw/taskapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package kg.geeks.hw.taskapp
+/*-Добавить в bottom nav - ProfileFragment
+-Добавить круглую картинку и editText
+-Переписать код с урока*/
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kg.geeks.hw.taskapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications,
+                R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+//        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+//            navView.isVisible = destination.id != R.id.navigation_profile
+//        }
+    }
+}
